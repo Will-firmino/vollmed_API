5e189b38d19cb9eb16a2696e93c9cc194a3429cc
William Firmino <prof.williamfirmino@gmail.com>
2026-09-11 09:32:02 -0300

    Alterações do dia 11/09/2026 antes do intervalo.

--- src/main/java/br/com/sistema/api/controller/MedicoController.java
+++ src/main/java/br/com/sistema/api/controller/MedicoController.java
@@ -2,6 +2,7 @@ package br.com.sistema.api.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,16 +10,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("medico")
 public class MedicoController {
 
+    @PostMapping("/cadastro")  // localhost:8080/medico/cadastro
+    public void cadastrarMedico(@RequestBody String dados) {
+        System.out.println(dados);
+    }
+
     // GET Request -> Response -> Ex: Tela home
     @GetMapping("/") // Aponta para localhost:8080/medico
     public String exibirHome() {
         return "a";
     };
 
-    @PostMapping("/cadastro") 
-    public String cadastrarMedico(String dados) {
-        return dados;
-    }
+
 
 
     // GET/POST Request -> Response -> Ex: Cadastrar Medico. GET Exibe tela de cadastro e o POST é chamado quando o botão enviar é clicado.
---
+++ src/main/java/br/com/sistema/api/model/medico/DadosCadastroMedico.java
@@ -0,0 +1,14 @@
+package br.com.sistema.api.model.medico;
+
+import br.com.sistema.api.model.endereco.DadosCadastroEndereco;
+
+// DTO do cadastro das informações do médico
+public record DadosCadastroMedico(
+        String nome,
+        String email,
+        String telefone,
+        String crm,
+        Especialidade especialidade,
+        DadosCadastroEndereco endereco) {
+
+}
